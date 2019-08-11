d98a5910a0d0f160d539e4075b69a06b9dba1710
Yulien Cabrera <yulienc@gmail.com>
2019-08-11 17:53:58 -0400

    why filter

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { UserData } from './app.component';
+import { filter } from 'minimatch';
 
 @Pipe({
   name: 'search'
@@ -10,7 +11,30 @@ export class SearchPipe implements PipeTransform {
     if (!users || !Searchname) {
       return users;
     }
-return users.filter(user=>user.name.indexOf(Searchname) !==-1);
+    return users.filter(user => user.name.indexOf(Searchname) !== -1);
+    // let result = []
+    // for (let index = 0; index < users.length; index++) {
+    //   const user = users[index];
+    //   if(user.name.indexOf(Searchname) !== -1) {
+    //     result.push(user)
+    //   }
+    // }
+    // return result;
+
+    // return filter(users, function(user) { return user.name.indexOf(Searchname) !== -1});
   }
 
+  
+
 }
+
+// function filter(array, predicate ) {
+//   let result = []
+//   for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     if( predicate(element)) {
+//       result.push(element)
+//     }
+//   }
+//   return result;
+// }
